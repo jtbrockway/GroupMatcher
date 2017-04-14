0531a06ae35e8b5e10d2e5c340c084b286f403d5
Jack Brockway <jtbrockway22@gmail.com>
2017-04-14 12:43:08 -0700

    Prettied up input file

--- input.py
+++ input.py
@@ -4,10 +4,37 @@ file = ""
 def set_file(filename):
     file = filename
 
+def get_intent(arr,lst):
+    #Function that grabs the intent of the student for the class from CSV
+    #Takes in an array read in from CSV and appends to the final list that the student info will be placed in
+    if((arr[2][-1])=='s'):
+        lst.append(0)
+    elif((arr[2][-1])=='A'):
+        lst.append(1)
+    else:
+        lst.append(2)
+
+def get_lang(arr, lst):
+    #Function that grabs the languages a student knows from CSV
+    #Takes in an array read in from CSV and appends to the final list that the student info will be placed in
+    langlist = arr[3].split(";")
+    lst.append(langlist)
+
+def get_times(arr, lst):
+    monlist = arr[4].split(';')
+    tulist = arr[5].split(';')
+    wedlist = arr[6].split(';')
+    thlist = arr[7].split(';')
+    frilist = arr[8].split(';')
+    lst.append(monlist)
+    lst.append(tulist)
+    lst.append(wedlist)
+    lst.append(thlist)
+    lst.append(frilist)
+
 def read_csv(filename):
     student_arr = []
     with open(filename) as csvfile:
-        #blank = fgetcsv(filename)
         flagger = False
         for line in csvfile:
             if flagger == False:
@@ -17,32 +44,12 @@ def read_csv(filename):
             linesplit = line.split(',')
             del linesplit[0]
             finlist = []
-            finlist.append(linesplit[0])
-            finlist.append(linesplit[1])
-            if((linesplit[2][-1])=='s'):
-                finlist.append(0)
-            elif((linesplit[2][-1])=='A'):
-                finlist.append(1)
-            else:
-                finlist.append(2)
-            langlist = linesplit[3].split(';')
-            finlist.append(langlist)
-            monlist = linesplit[4].split(';')
-            tulist = linesplit[5].split(';')
-            wedlist = linesplit[6].split(';')
-            thlist = linesplit[7].split(';')
-            frilist = linesplit[8].split(';')
-            finlist.append(monlist)
-            finlist.append(tulist)
-            finlist.append(wedlist)
-            finlist.append(thlist)
-            finlist.append(frilist)
-            
+            finlist.append(linesplit[0]) #Get first name
+            finlist.append(linesplit[1]) #Get last name
+            get_intent(linesplit,finlist)#Get intent
+            get_lang(linesplit, finlist) #Get the languages
+            get_times(linesplit, finlist)
             student_arr.append(finlist)
-
-        print(student_arr)
-
-            
 
 read_csv('response.csv')
 
