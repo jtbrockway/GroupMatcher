6856131a9f969ae4245f186542bb047a0e7f0a82
Jack Brockway <jtbrockway22@gmail.com>
2017-04-20 14:49:41 -0700

    Initial output functions created (maybe finished)

---
+++ output.py
@@ -0,0 +1,21 @@
+import csv
+
+def group_names(group):
+    printGroup = []
+    for i in range(len(group)):
+        first = student_arr[group[i]][0]
+        last = student_arr[group[i]][1]
+        name = first + last
+        printGroup.append(name)
+    return printGroup
+
+def write_csv():
+    with open('groups.csv', 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter = ' ',
+                                quotechar=',', quoting = csv.QUOTE_MINIMAL)
+        
+        for i in range(len(final_groups)):
+            group = group_names(final_groups[i]) 
+            spamwriter.writerow(['Group']+ [i] + [":"] + group)
+
+write_csv()
