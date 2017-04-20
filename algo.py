35e9a5b4273033169948de99813b7b13e7315d66
Jack Brockway <jtbrockway22@gmail.com>
2017-04-20 16:33:56 -0700

    more algo stuff

--- algo.py
+++ algo.py
@@ -4,26 +4,36 @@ from itertools import combinations
 student_arr = []
 pos_groups = []
 groupnums = []
+final_groups = []
 groupsize = 3
 stunum = 9
 priorities = ['t', 'i', 'l']
 student_arr = read_csv('response.csv')
 
-def tracker(groups):
+def tracker(groups, deleted):
+    global final_groups
     retarray = []
     for i in range(stunum):
         retarray.append(0)
     for group in groups:
-    	for person in group:
-        	retarray[person] += 1
+        for person in group:
+            retarray[person] += 1
     retlist = []
-    for i in range (len(retarray)):
-    	if retarray[i] == 0:
-    		retlist.append(i)
-    return retlist
+    for i in range (stunum):
+        if retarray[i] == 0:
+            for item in deleted[i]:
+                groups.append(item)
+        if retarray[i] == 1:
+            for group in groups:
+                if i in group:
+                    final_groups.append(group)
+                    groups.remove(group)
+    return groups
 
 def time(posgroup):
     deletedgroups = []
+    for i in range(stunum):
+        deletedgroups.append([])
     counter = 0
     newposgroups = []
     for group in posgroup:
@@ -44,13 +54,17 @@ def time(posgroup):
             counter += 1
             newposgroups.append(group)
         else:
-                deletedgroups.append(group)
+            for person in group:
+                deletedgroups[person].append(group)
     print ('time', counter)
+    newpostgroups = tracker(newposgroups, deletedgroups)
     global pos_groups
     pos_groups = newposgroups
     
 def intention(groups):
     deletedgroups = []
+    for i in range(stunum):
+        deletedgroups.append([])
     newposgroups = []
     counter = 0
     for group in groups:
@@ -58,7 +72,9 @@ def intention(groups):
             counter = counter + 1
             newposgroups.append(group)
         else:
-                deletedgroups.append(group)
+            for person in group:
+                deletedgroups[person].append(group)
+    newposgroups = tracker(newposgroups, deletedgroups)
     global pos_groups
     pos_groups = newposgroups
     print('intention counter ', counter)
@@ -66,6 +82,8 @@ def intention(groups):
 def language(groups):
     counter = 0
     deletedgroups = []
+    for i in range(stunum):
+        deletedgroups.append([])
     global pos_groups
     newposgroups = []
     for group in groups:
@@ -78,7 +96,9 @@ def language(groups):
                     counter += 1
                     break
         if(flagger):
-                deletedgroups.append(group)
+            for person in group:
+                deletedgroups[person].append(group)
+    newposgroups = tracker(newposgroups, deletedgroups)
     print('language', counter)
     pos_groups = newposgroups
 
@@ -98,8 +118,9 @@ def driver():
             intention(pos_groups)
         if charnew == 'l':
             language(pos_groups)
-        print(tracker(pos_groups))
         print(pos_groups)
+        print("--------")
+        print(final_groups)
 
     #base cases multiple group possiblities after all paramters filtered
 
