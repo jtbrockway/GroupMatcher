0c591c75240c58ad139242b1f4df578c2dd6e09b
Jack Brockway <jtbrockway22@gmail.com>
2017-04-25 15:14:24 -0700

    MVP almost complete

--- outputToFile.py
+++ outputToFile.py
@@ -1,21 +1,22 @@
 import csv
 
+
 def group_names(group, student_arr):
     printGroup = []
-    for i in range(len(group)):
+    for i in range(len(group)-1):
         first = student_arr[group[i]][0]
         last = student_arr[group[i]][1]
-        name = first + last
+        name = " ".join([first,last])
         printGroup.append(name)
+    printGroup.append(student_arr[group[-1]][0] + ' ' + student_arr[group[-1]][1])
     return printGroup
 
 def write_csv(final_groups, student_arr):
-    with open('Groups.csv', 'w', newline='') as csvfile:
-        spamwriter = csv.writer(csvfile, delimiter = ' ',
-                                quotechar=',', quoting = csv.QUOTE_MINIMAL)
+    with open('Groups.csv', 'w') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter = ',')
         
         for i in range(len(final_groups)):
+            j = i+1
             group = group_names(final_groups[i],student_arr) 
-            spamwriter.writerow(['Group']+ [i] + [":"] + group)
-
-
+            spamwriter.writerow(['Group'  + " " + str(j) + ":"] + group)
+   
